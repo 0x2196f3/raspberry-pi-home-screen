6292a0390eae05fce613ac9468edbf258adac52a
user0 <user0@private.com>
2023-12-26 21:18:35 +0800

    .

---
+++ config.py
@@ -0,0 +1,3 @@
+url = "http://192.168.2.2:9003"
+username = "user"
+password = "2196f3"
---
+++ main.py
@@ -0,0 +1,38 @@
+import platform
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+import config
+
+
+def login(driver, username, password):
+    time.sleep(2)
+    inputs = driver.find_elements(by=By.TAG_NAME, value="input")
+    inputs[0].send_keys(username)
+    inputs[1].send_keys(password)
+
+    driver.execute_script("document.getElementsByClassName('action')[0].children[0].click();")
+
+    time.sleep(10)
+    
+    driver.execute_script("document.getElementsByTagName('home-assistant')[0].shadowRoot.childNodes[2].shadowRoot.childNodes[2].children[0].shadowRoot.childNodes[3].children[0].click()")
+
+
+
+if __name__ == "__main__":
+    options = webdriver.FirefoxOptions()
+
+    options.binary_location = "/usr/lib/firefox-esr/firefox-esr"
+
+    driver = webdriver.Firefox(options=options)    
+    
+    driver.fullscreen_window()
+
+    url = config.url
+
+    driver.get(url)
+
+    login(driver, config.username, config.password)
